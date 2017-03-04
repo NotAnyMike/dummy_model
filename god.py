3c64ef8a95b3ad8c4563a1c19d4d946669c87a85
Mike Woodcock <mwc1.0@hotmail.com>
2017-03-04 16:18:54 -0500

    Creating pseudo code for event 1

--- god.py
+++ god.py
@@ -7,6 +7,14 @@ class God(object):
         self._next_event = 0;
 
         #init everthing
+        
+    def _ask_uber(self):
+        #1. add Person to a list of watingListOfPersons
+        #2. if are Drivers free then
+        #2.1 if are Drivers free in less then 5km and are Uber or/and Taxis
+        #2.2 create an event of type 2 for the same time of simulation
+        #3. Create the next event of this type (1)
+        pass
 
     def startEverything(self):
         numberOfUbers = self._numberOfPersons/self._ratioPersonsDrivers*self._ratioUberCabs
